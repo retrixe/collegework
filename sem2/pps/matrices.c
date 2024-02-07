a38b3862ff54cad383500730a249c9297ec258a3
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2024-02-08 02:23:05 +0530

    Fix folder names, improve matrix assgn

--- sem2/pps/matrices.c
+++ sem2/pps/matrices.c
@@ -40,12 +40,39 @@ int main() {
     printf("\n");
   }
 
-  printf("===== Result =====\n");
-  for (int row = 0; row < rows; row++) {
-    for (int col = 0; col < cols; col++) {
-      printf("%d\t", matrix1[row][col] + matrix2[row][col]);
+  while (1) {
+    printf("==================\n");
+    printf("Options:\n");
+    printf("1 - Add\n2 - Subtract\n3 - Transpose matrix 1\n4 - Invert matrix 1\n0 - Exit\n");
+    printf("Enter option: ");
+    int option;
+    scanf("%d", &option);
+    if (option > 4 || option < 0) {
+      printf("ERROR: Invalid option entered!\n");
+      continue;
+    } else if (option == 0) {
+      printf("Exiting...\n");
+      return 0;
     }
-    printf("\n");
+
+    if (option == 4) {
+      // Invert matrix 1 and store in matrix2
+    }
+
+    printf("===== Result =====\n");
+    for (int row = 0; row < rows; row++) {
+      for (int col = 0; col < cols; col++) {
+        if (option == 1)
+          printf("%d\t", matrix1[row][col] + matrix2[row][col]);
+        if (option == 2)
+          printf("%d\t", matrix1[row][col] - matrix2[row][col]);
+        if (option == 3)
+          printf("%d\t", matrix1[col][row]);
+        if (option == 4) {}
+// TODO: Perform inversion          printf("%d\t", matrix1[row][col] + matrix2[row][col]);
+      }
+      printf("\n");
+    }
+    return 0;
   }
-  return 0;
 }

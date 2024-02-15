aed932a961850c64d3abcf342c3b920a380661e0
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2024-02-15 15:58:06 +0530

    sem2/pps: update matrix code

--- sem2/pps/matrices.c
+++ sem2/pps/matrices.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 
 int main() {
-  int rows, cols;
+  int rows, cols, option;
   printf("Enter number of rows (up to 10): ");
   scanf("%d", &rows);
   printf("Enter number of columns (up to 10): ");
   scanf("%d", &cols);
   int matrix1[rows][cols], matrix2[rows][cols];
+
+  while (1) {
+    printf("Options:\n");
+    printf("1 - Add\n2 - Subtract\n3 - Transpose matrix\n0 - Exit\n");
+    printf("Enter option: ");
+    scanf("%d", &option);
+    if (option > 3 || option < 0) {
+      printf("ERROR: Invalid option entered!\n");
+      continue;
+    } else if (option == 0) {
+      printf("Exiting...\n");
+      return 0;
+    } else {
+      break;
+    }
+  }
 
   printf("===== Matrix 1 =====\n");
   for (int row = 0; row < rows; row++) {
@@ -16,11 +32,13 @@ int main() {
     }
   }
 
-  printf("===== Matrix 2 =====\n");
-  for (int row = 0; row < rows; row++) {
-    for (int col = 0; col < cols; col++) {
-      printf("Enter number for row %d, col %d: ", row + 1, col + 1);
-      scanf("%d", &matrix2[row][col]);
+  if (option == 1 || option == 2) {
+    printf("===== Matrix 2 =====\n");
+    for (int row = 0; row < rows; row++) {
+      for (int col = 0; col < cols; col++) {
+        printf("Enter number for row %d, col %d: ", row + 1, col + 1);
+        scanf("%d", &matrix2[row][col]);
+      }
     }
   }
 
@@ -32,41 +50,32 @@ int main() {
     printf("\n");
   }
 
-  printf("===== Matrix 2 =====\n");
-  for (int row = 0; row < rows; row++) {
-    for (int col = 0; col < cols; col++) {
-      printf("%d\t", matrix2[row][col]);
-    }
-    printf("\n");
-  }
-
-  while (1) {
-    printf("==================\n");
-    printf("Options:\n");
-    printf("1 - Add\n2 - Subtract\n3 - Transpose matrix 1\n0 - Exit\n");
-    printf("Enter option: ");
-    int option;
-    scanf("%d", &option);
-    if (option > 4 || option < 0) {
-      printf("ERROR: Invalid option entered!\n");
-      continue;
-    } else if (option == 0) {
-      printf("Exiting...\n");
-      return 0;
-    }
-
-    printf("===== Result =====\n");
+  if (option == 1 || option == 2) {
+    printf("===== Matrix 2 =====\n");
     for (int row = 0; row < rows; row++) {
       for (int col = 0; col < cols; col++) {
-        if (option == 1)
-          printf("%d\t", matrix1[row][col] + matrix2[row][col]);
-        if (option == 2)
-          printf("%d\t", matrix1[row][col] - matrix2[row][col]);
-        if (option == 3)
-          printf("%d\t", matrix1[col][row]);
+        printf("%d\t", matrix2[row][col]);
       }
       printf("\n");
     }
-    return 0;
   }
+
+  printf("===== Result =====\n");
+  for (int row = 0; row < rows; row++) {
+    for (int col = 0; col < cols; col++) {
+      switch (option) {
+        case 1:
+          printf("%d\t", matrix1[row][col] + matrix2[row][col]);
+          break;
+        case 2:
+          printf("%d\t", matrix1[row][col] - matrix2[row][col]);
+          break;
+        case 3:
+          printf("%d\t", matrix1[col][row]);
+          break;
+      }
+    }
+    printf("\n");
+  }
+  return 0;
 }

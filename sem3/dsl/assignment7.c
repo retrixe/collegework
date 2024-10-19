7ceeea19b1d97a043a0e1a7a09618aecb5cff596
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2024-10-20 02:23:33 +0530

    sem3/dsl: add assignment 7

--- sem3/dsl/assignment7.c
+++ sem3/dsl/assignment7.c
@@ -73,7 +73,7 @@ void insert(bstnode* root) {
 				temp->left = curr;
 				break;
 			} else {
-				temp = temp->right;
+				temp = temp->left;
 			}
 		} else {
 			if (temp->right == NULL) {
@@ -87,7 +87,7 @@ void insert(bstnode* root) {
 }
 
 void deletenode(bstnode** root) {
-	printf("Enter keyword to search: ");
+	printf("Enter keyword to delete: ");
 	char keyword[32];
 	fgets(keyword, 32, stdin);
 	keyword[strcspn(keyword, "\n")] = 0;
@@ -116,8 +116,8 @@ void deletenode(bstnode** root) {
 		else if (curr->right == NULL) { *root = (*root)->left; }
 		else {
 			temp = (*root)->left;
-			*root = (*root)->right;
 			bstnode* s = (*root)->right;
+			*root = (*root)->right;
 			while (s->left != NULL) { s = s->left; }
 			s->left = temp;
 		}
@@ -232,6 +232,20 @@ bstnode* dequeue() {
 	return value;
 }
 
+
+void mirror_nr(bstnode* root) {
+	bstnode* temp = root;
+	enqueue(temp);
+	while (!queueIsEmpty()) {
+		temp = dequeue();
+		bstnode* prevleft = temp->left;
+		temp->left = temp->right;
+		temp->right = prevleft;
+		if (temp->left != NULL) enqueue(temp->left);
+		if (temp->right != NULL) enqueue(temp->right);
+	}
+}
+
 void display_bfs(bstnode* root) {
 	bstnode* temp = root;
 	enqueue(temp);
@@ -240,6 +254,118 @@ void display_bfs(bstnode* root) {
 		printf("%s (%s)\n", temp->keyword, temp->definition);
 		if (temp->left != NULL) enqueue(temp->left);
 		if (temp->right != NULL) enqueue(temp->right);
+	}
+}
+
+bstnode* copy_r(bstnode* root) {
+	bstnode* temp = NULL;
+	if (root != NULL) {
+		temp = (bstnode*)malloc(sizeof(bstnode));
+		strcpy(temp->keyword, root->keyword);
+		strcpy(temp->definition, root->definition);
+		temp->left = copy_r(root->left);
+		temp->right = copy_r(root->right);
+	}
+	return temp;
+}
+
+#define MAX 64
+bstnode* stack1[MAX];
+int stackTop1 = -1;
+
+int stackIsFull1() {
+	return stackTop1 == MAX - 1;
+}
+
+int stackIsEmpty1() {
+	return stackTop1 == -1;
+}
+
+void stackPush1(bstnode* element) {
+	if (stackIsFull1()) {
+		printf("Stack is full\n");
+	} else {
+		stack1[++stackTop1] = element;
+	}
+}
+
+bstnode* stackPop1() {
+	if (stackIsEmpty1(stackTop1)) {
+		return NULL;
+	} else {
+		return stack1[stackTop1--];
+	}
+}
+
+bstnode* stack2[MAX];
+int stackTop2 = -1;
+
+int stackIsFull2() {
+	return stackTop2 == MAX - 1;
+}
+
+int stackIsEmpty2() {
+	return stackTop2 == -1;
+}
+
+void stackPush2(bstnode* element) {
+	if (stackIsFull2()) {
+		printf("Stack 2 is full\n");
+	} else {
+		stack2[++stackTop2] = element;
+	}
+}
+
+bstnode* stackPop2() {
+	if (stackIsEmpty2(stackTop2)) {
+		return NULL;
+	} else {
+		return stack2[stackTop2--];
+	}
+}
+
+bstnode* copy_nr(bstnode* root2) {
+	bstnode* root1 = (bstnode*)malloc(sizeof(bstnode));
+	bstnode* temp1 = root1;
+	bstnode* temp2 = root2;
+	strcpy(temp1->keyword, temp2->keyword);
+	strcpy(temp1->definition, temp2->definition);
+	while (1) {
+		while (temp2 != NULL) {
+			if (temp2->left != NULL) {
+				temp1->left = (bstnode*)malloc(sizeof(bstnode));
+				temp1->left->left = NULL; temp1->left->right = NULL;
+				strcpy(temp1->left->keyword, temp2->left->keyword);
+				strcpy(temp1->left->definition, temp2->left->definition);
+			}
+			if (temp2->right != NULL) {
+				temp1->right = (bstnode*)malloc(sizeof(bstnode));
+				temp1->right->left = NULL; temp1->right->right = NULL;
+				strcpy(temp1->right->keyword, temp2->right->keyword);
+				strcpy(temp1->right->definition, temp2->right->definition);
+			}
+			stackPush1(temp1);
+			stackPush2(temp2);
+			temp1 = temp1->left;
+			temp2 = temp2->left;
+		}
+		if (stackIsEmpty1()) break;
+		temp1 = stackPop1();
+		temp2 = stackPop2();
+		temp1 = temp1->right;
+		temp2 = temp2->right;
+	}
+	return root1;
+}
+
+void deletetree(bstnode* root) {
+	bstnode* temp = NULL;
+	if (root != NULL) {
+		deletetree(root->left);
+		deletetree(root->right);
+		root->left = NULL;
+		root->right = NULL;
+		free(root);
 	}
 }
 
@@ -260,26 +386,41 @@ int main() {
 	root->definition[strcspn(root->definition, "\n")] = 0;
 
 	create(root);
+	bstnode* copy;
 	while (1) {
 		printf("==========\n");
 		printf("1. Inorder traversal (recursive)\n");
 		printf("2. Breadth traversal\n");
 		printf("3. Insert a new word\n");
 		printf("4. Delete a word\n");
-		printf("5. Copy binary search tree\n");
-		printf("6. Mirror the tree\n");
-		printf("7. Search for a word (recursive)\n");
-		printf("8. Search for a word (non-recursive)\n");
+		printf("5. Copy tree (recursive)\n");
+		printf("6. Copy tree (non-recursive)\n");
+		printf("7. Mirror the tree (recursive)\n");
+		printf("8. Mirror the tree (non-recursive)\n");
+		printf("9. Search for a word (recursive)\n");
+		printf("10. Search for a word (non-recursive)\n");
 		printf("Enter choice: ");
 		char choice[4]; fgets(choice, 4, stdin);
-		switch (choice[0]) {
-			case '1': inorder_r(root); break;
-			case '2': display_bfs(root); break;
-			case '3': insert(root); break;
-			case '4': deletenode(&root); break;
-			case '6': mirror_r(root); break;
-			case '7': search_r(root); break;
-			case '8': search_nr(root); break;
+		int ch; sscanf(choice, "%d", &ch);
+		switch (ch) {
+			case 1: inorder_r(root); break;
+			case 2: display_bfs(root); break;
+			case 3: insert(root); break;
+			case 4: deletenode(&root); break;
+			case 5:
+			case 6:
+				if (ch == 5) copy = copy_r(root);
+				else copy = copy_nr(root);
+				printf("Inorder traversal of copy:\n");
+				inorder_r(copy);
+				printf("Breadth first traversal of copy::\n");
+				display_bfs(copy);
+				deletetree(copy);
+				break;
+			case 7: mirror_r(root); break;
+			case 8: mirror_r(root); break;
+			case 9: search_r(root); break;
+			case 10: search_nr(root); break;
 			default: printf("Incorrect choice!\n");
 		}
 	}

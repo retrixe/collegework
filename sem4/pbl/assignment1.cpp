9f112b1cd6c714cde67715abfe2bf20e710cb50e
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-01-15 12:30:39 +0530

    sem4/pbl: finish assignment 1

--- sem4/pbl/assignment1.cpp
+++ sem4/pbl/assignment1.cpp
@@ -17,6 +17,12 @@ class User {
 	}
 
 	friend class Records;
+
+	void display() {
+		cout << "Mobile no: " << mobileNo << endl;
+		cout << "Name: " << name << endl;
+		cout << "Bill amount: " << billAmount << endl;
+	}
 };
 
 #define MAX 100
@@ -37,6 +43,53 @@ class Records {
 
 	void heapSort();
 	void adjust(int n, int i);
+
+	void quickSort(int low, int high);
+	int partition(int low, int high);
+
+	void linearSearch(long int mobileNo) {
+		for (int i = 0; i < n; i++) {
+			if (users[i].mobileNo == mobileNo) {
+				users[i].display();
+				return;
+			}
+		}
+		cout << "No user with this mobile no found!" << endl;
+	}
+
+	void binarySearchNonRecursive(long int mobileNo, int low, int high) {
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if (users[mid].mobileNo == mobileNo) {
+				users[mid].display();
+				return;
+			} else if (mobileNo > users[mid].mobileNo) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		cout << "No user with this mobile no found!" << endl;
+	}
+
+	void binarySearchRecursive(long int mobileNo, int low, int high) {
+		if (low <= high) {
+			int mid = (low + high) / 2;
+			if (users[mid].mobileNo == mobileNo) {
+				users[mid].display();
+			} else if (mobileNo > users[mid].mobileNo) {
+				binarySearchRecursive(mobileNo, low, mid - 1);
+			} else {
+				binarySearchRecursive(mobileNo, mid + 1, high);
+			}
+			return;
+		}
+		cout << "No result found!" << endl;
+	}
+
+	void binarySearchRecursive(long int mobileNo) {
+		binarySearchRecursive(mobileNo, 0, n);
+	}
 };
 
 void Records::accept() {
@@ -54,9 +107,7 @@ void Records::accept() {
 void Records::display() {
 	for (int i = 0; i < n; i++) {
 		cout << "===== User #" << i + 1 << " =====" << endl;
-		cout << "Mobile no: " << users[i].mobileNo << endl;
-		cout << "Name: " << users[i].name << endl;
-		cout << "Bill amount: " << users[i].billAmount << endl;
+		users[i].display();
 	}
 }
 
@@ -78,18 +129,40 @@ void Records::adjust(int n, int i) {
 	while (2 * i + 1 <= n) {
 		int j = 2 * i + 1;
 
-		if (j + 1 <= n && users[j + 1].mobileNo > users[j].mobileNo) {
+		if (j + 1 <= n && users[j + 1].billAmount > users[j].billAmount) {
 			j = j + 1;
 		}
 
-		if (users[i].mobileNo >= users[j].mobileNo) break;
-		else {
+		if (users[i].billAmount >= users[j].billAmount) {
+			break;
+		} else {
 			User temp = users[i];
 			users[i] = users[j];
 			users[j] = temp;
 			i = j;
 		}
 	}
+}
+
+void Records::quickSort(int low, int high) {
+	if (low < high) {
+		int pivot = partition(low, high);
+		quickSort(low, pivot - 1);
+		quickSort(pivot + 1, high);
+	}
+}
+
+int Records::partition(int low, int high) {
+	auto x = users[high];
+	int i = low - 1;
+	for (int j = low; j < high; j++) {
+		if (users[j].mobileNo >= x.mobileNo) {
+			i++;
+			swap(users[i], users[j]);
+		}
+	}
+	swap(users[i + 1], users[high]);
+	return i + 1;
 }
 
 int main() {
@@ -104,7 +177,11 @@ int main() {
 		int option;
 		cout << "===== Options =====" << endl;
 		cout << "1. Display" << endl;
-		cout << "2. Heap sort" << endl;
+		cout << "2. Heap sort by bill amount" << endl;
+		cout << "3. Quick sort by mobile number" << endl;
+		cout << "4. Linear search by mobile number" << endl;
+		cout << "5. Recursive binary search by mobile number" << endl;
+		cout << "6. Non-recursive binary search by mobile number" << endl;
 		cout << "0. Exit" << endl;
 		cout << "Enter option: ";
 		cin >> option;
@@ -113,7 +190,19 @@ int main() {
 			records.display();
 		} else if (option == 2) {
 			records.heapSort();
-			cout << "Heap sorted by mobile number!" << endl;
+			cout << "Heap sorted by bill amount!" << endl;
+		} else if (option == 3) {
+			records.quickSort(0, n - 1);
+			cout << "Quick sorted by mobile number!" << endl;
+		} else if (option >= 4 && option <= 6) {
+			records.quickSort(0, n - 1);
+			cout << "Enter mobile number: ";
+			long int mobileNo;
+			cin >> mobileNo;
+			cout << "Result:" << endl;
+			if (option == 4) records.linearSearch(mobileNo);
+			else if (option == 5) records.binarySearchRecursive(mobileNo);
+			else records.binarySearchNonRecursive(mobileNo, 0, n);
 		} else {
 			cout << "Invalid option selected!" << endl;
 		}

a95e382c350f9d9185fc56895081fe43fa5f5aaf
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-09-04 15:21:00 +0530

    sem5/bdt: add assignment 5 sample

---
+++ sem5/bdt/assignment5_sample/WordMapper.java
@@ -0,0 +1,20 @@
+package xyz.retrixe;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while (tokenizer.hasMoreTokens()) {
+            value.set(tokenizer.nextToken());
+            context.write(value, new IntWritable(1));
+        }
+    }
+}
